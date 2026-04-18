complete the adaptivity for az gradient and minor fixes

optimizer.py
@@ -296,7 +296,7 @@ class ParaflowS(Optimizer):
         '''
         super().__init__('ParaflowS',beta, v, W, bcs, h)
 
-    def compile(self, u0, tau_f, tau_g, E_ref, grad_type_coarse, grad_type_fine, Nf, Ng = 100):
+    def compile(self, u0, E_ref, grad_type_coarse, grad_type_fine, Nf, Ng = 100, tau_f = None, tau_g = None):
         '''
         Docstring for compile
         
@@ -455,38 +455,38 @@ class ParaflowS(Optimizer):
         :param method_name (string): name of the gradient method used
         '''
 
-        fig, ax = plt.subplots(2,1, figsize=(5,10))
+        fig, ax = plt.subplots(1,1, figsize=(10,10))
         fig.suptitle(f'{self.name_optimizer} with fine:{self.fine_solver.name} and coarse: {self.coarse_solver.name},\n h={self.h}, beta={float(self.beta)}, tau_f={float(self.fine_solver.tau)}, tau_c={float(self.coarse_solver.tau)}')
 
         offset = 0
         for element in self.history_E:
             if offset == 0:
                 times = np.array(range(0,self.Nf)) * float(self.fine_solver.tau) + offset
-                ax[0].semilogy(times, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf)], marker='o', c = 'orange', label = 'Fine solver')
-                ax[1].semilogy(times, [element[i] for i in range(self.Nf)], marker='o', c = 'orange', label = 'Fine solver')
+                ax.semilogy(times, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf)], marker='o', linewidth = 5, markersize = 10, c = '#e67e22', label = 'Fine solver')
+                #ax[1].semilogy(times, [element[i] for i in range(self.Nf)], marker='o', c = '#e67e22', label = 'Fine solver')
                 offset += (self.Nf - 1) * float(self.fine_solver.tau)
-                ax[0].semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf, len(element))], marker='x', c = 'blue', label = 'Correction')
-                ax[1].semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [element[i] for i in range(self.Nf, len(element))], marker='x', c = 'blue', label = 'Correction')
+                ax.semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf, len(element))], marker='o', linewidth = 5, markersize = 10, c = '#2c3e50', label = 'Correction')
+                #ax[1].semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [element[i] for i in range(self.Nf, len(element))], marker='x', c = '#2c3e50', label = 'Correction')
                 offset += (len(element) - self.Nf - 1) * float(self.coarse_solver.tau) + float(self.fine_solver.tau)
             else:
                 times = np.array(range(0,self.Nf)) * float(self.fine_solver.tau) + offset
-                ax[0].semilogy(times, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf)], marker='o', c = 'orange')
-                ax[1].semilogy(times, [element[i] for i in range(self.Nf)], marker='o', c = 'orange')
+                ax.semilogy(times, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf)], marker='o', linewidth = 5, markersize = 10, c = '#e67e22')
+                #ax[1].semilogy(times, [element[i] for i in range(self.Nf)], marker='o', c = '#e67e22')
                 offset += (self.Nf - 1) * float(self.fine_solver.tau)
-                ax[0].semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf, len(element))], marker='x', c = 'blue')
-                ax[1].semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [element[i] for i in range(self.Nf, len(element))], marker='x', c = 'blue')
+                ax.semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [abs(element[i] - self.E_ref) / self.E_ref for i in range(self.Nf, len(element))], marker='o', linewidth = 5, markersize = 10, c = '#2c3e50')
+                #ax[1].semilogy(np.array(range(0,len(element) - self.Nf )) * float(self.coarse_solver.tau) + offset, [element[i] for i in range(self.Nf, len(element))], marker='x', c = '#2c3e50')
                 offset += (len(element) - self.Nf - 1) * float(self.coarse_solver.tau) + float(self.fine_solver.tau)
-        ax[0].set_xlabel('Iteration')
-        ax[0].set_ylabel('Relative Error on Energy')
-        ax[0].set_title('Convergence History')
-        ax[0].grid(True)
-        ax[0].legend()
+        ax.set_xlabel('Iteration', fontsize = 30)
+        ax.set_ylabel('Relative Error on Energy', fontsize = 30)
+        ax.set_title('Convergence History', fontsize = 30)
+        ax.grid(True)
+        ax.legend()
 
-        ax[1].set_xlabel('Iteration')
-        ax[1].set_ylabel('Energy')
-        ax[1].set_title('Energy History')
-        ax[1].grid(True)
-        ax[1].legend()
+        # ax[1].set_xlabel('Iteration')
+        # ax[1].set_ylabel('Energy')
+        # ax[1].set_title('Energy History')
+        # ax[1].grid(True)
+        # ax[1].legend()
 
         if show:
             plt.show()

test/plot_comparison_2d.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+import numpy as np
 
 # Configuration
-case_folder = Path(__file__).parent / 'case_test2'
+case_folder = Path(__file__).parent / 'case_test3'
 
 # Extract test case number from folder name
 test_case_num = case_folder.name.replace('case_test', '')
@@ -23,6 +24,7 @@ print(f"Unique N_fine values: {N_fine}")
 N_coarse = df_pf_dtxNf['N_coarse'].unique()
 opt = df_pf_dtxNf['fine_operator'].unique()
 
+df_gd_ada = df_gd[df_gd['optimizer_name'] == 'az_ada']
 
 color = {'L2_P': '#1f77b4', 'az': '#ff7f0e'}  # Blue for L2_P, Orange for az
 
@@ -81,6 +83,13 @@ for nf in N_fine:
                 ax.plot(subset_dtxNf_2['tau_fine'].values, it,
                     marker='d', linewidth=3, markersize=10, linestyle='--', color = '#d62728',
                     label=label)
+                df_pf_ada = pd.read_csv(case_folder / 'PFalpha_ada.csv')
+                ax.plot(subset_dtxNf_2['tau_fine'].values,
+                        (df_pf_ada['iterate_fine'] + df_pf_ada['iterate_coarse']).values.copy() * np.ones(subset_dtxNf_2['tau_fine'].values.shape),
+                        label = fr'$a_u$-PF: adaptive $\tau_F$')
+                ax.plot(subset_dtxNf_2['tau_fine'].values,
+                        df_gd_ada['iterate'].values.copy() * np.ones(subset_dtxNf_2['tau_fine'].values.shape),
+                        label = fr'$a_u$: adaptive $\tau_F$')
 
             else:
                 it = (subset_dtxNf_2x3['iterate_fine'] + subset_dtxNf_2x3['iterate_coarse']).values.copy()
@@ -156,9 +165,9 @@ for nf in N_fine:
             ax.tick_params(axis='both', which='minor', labelsize=30)
 
             plt.tight_layout()
-        # plt.show()
+        plt.show()
         # exit()
-        plt.savefig(case_folder / f'plot_{test_case_num}_comp_Nf{nf}_Nc{nc}.png', dpi=300)
-        plt.close()
+        # plt.savefig(case_folder / f'plot_{test_case_num}_comp_Nf{nf}_Nc{nc}.png', dpi=300)
+        # plt.close()
 
 print("Done!")

test/test_plot_2d.py
@@ -11,9 +11,9 @@ from pot_3 import RandomDisorderPotential
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from optimizer import Gradient_Descent, ParaflowS
 
-is_save_CSV = True
+is_save_CSV = False
 is_save_log = False
-is_save_plot = False
+is_save_plot = True
 
 folder = Path('~/Desktop/tesi/tesi/test').expanduser()
 
@@ -21,15 +21,15 @@ nx = 256
 # beta_v = [1000]#,100,1000]
 MaxIter = 1000
 toll = 1e-5
-test = '3' # '1', '2' or '3'
+test = '1' # '1', '2' or '3'
 
 methods_coarse = ['L2_P']# ['L2_P', 'az']
-methods_fine = ['L2_P', 'az']
+methods_fine = ['az']# ['L2_P', 'az']
 Nf_v = [4]#[2, 3, 4, 5, 6]#, 10]
 Ng_v = [10]#, 20]#, 100]
 
 if test == '1':
-    tau_v ={'az': [0.025, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1], 'L2_P': [0.005, 0.01, 0.05, 0.1, 0.5, 0.8, 1, 1.5, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100]} #L2_P
+    tau_v ={'az':[0.1]}#{'az': [0.025, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1], 'L2_P': [0.005, 0.01, 0.05, 0.1, 0.5, 0.8, 1, 1.5, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100], 'az_ada': [1]} #L2_P
     # tau_v = [0.025, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1]#az#1, 0.5, 0.25, 0.1, 0.05, 0.025] #[1, 0.5]
     #tau_v = [1.2,1.3,1.5,1.7,1.9]#az
     E_ref = {10: 0.79620688, 100: 1.97298868, 1000: 5.99303235}
@@ -40,9 +40,9 @@ if test == '1':
     filename_results_GD = str(case_folder / 'GD.csv')
     # filename_results_GD = str(case_folder / 'GD_conv.csv')
     # filename_results_PF = str(case_folder / 'PFm_dt.csv')
-    filename_results_PF = str(case_folder / 'PFalpha_dt.csv')
+    filename_results_PF = str(case_folder / 'PFalpha_ada.csv')
 elif test == '2':
-    tau_v = {'az':list(np.linspace(0.05, 1.7,19)), 'L2_P':[0.005, 0.01, 0.05, 0.1, 0.5, 0.8, 1, 1.5, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100]} #L2_P
+    tau_v = {'az':list(np.linspace(0.05, 1.7,19)), 'L2_P':[0.005, 0.01, 0.05, 0.1, 0.5, 0.8, 1, 1.5, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100], 'az_ada': [1]} #L2_P
 
     beta = 1000
     # tau_v = list(np.linspace(0.05, 1.7,19)) #az
@@ -51,9 +51,9 @@ elif test == '2':
     case_folder = folder / 'case_test2'
     case_folder.mkdir(parents=True, exist_ok=True)
     filename_results_GD = str(case_folder / 'GD.csv')
-    filename_results_PF = str(case_folder / 'PFalpha_dtxNf.csv')
+    filename_results_PF = str(case_folder / 'PFalpha_ada.csv')
 elif test == '3':
-    tau_v = {'az':list(np.linspace(0.5, 2.0, 15)), 'L2_P': [0.01, 0.05, 0.1, 0.5, 0.8, 1, 1.5, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100]} #L2_P
+    tau_v = {'az':list(np.linspace(0.5, 2.0, 15)), 'L2_P': [0.01, 0.05, 0.1, 0.5, 0.8, 1, 1.5, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100], 'az_ada': [1]} #L2_P
     # [0.05,0.1]
     beta = 10
     # tau_v = list(np.linspace(0.5, 2.0, 15)) #az
@@ -61,8 +61,8 @@ elif test == '3':
 
     case_folder = folder / 'case_test3'
     case_folder.mkdir(parents=True, exist_ok=True)
-    # filename_results_GD = str(case_folder / 'GD.csv')
-    filename_results_PF = str(case_folder / 'PFalpha_dt.csv')
+    filename_results_GD = str(case_folder / 'GD.csv')
+    filename_results_PF = str(case_folder / 'PFalpha_ada.csv')
 else:
     NameError('test must be 1, 2 or 3')
 
@@ -159,7 +159,7 @@ for name_fine in methods_fine:
 
     # if not(done):
         for tau in tqdm(tau_v[name_fine]):
-            # optim_GD.compile(u0, tau, E_ref[beta],grad_type = name_fine)
+            # optim_GD.compile(u0, E_ref[beta],grad_type = name_fine, tau = tau)
 
             # filename = 'GD'+ name_fine + '_tau' + str(tau)
             # if is_save_log:
@@ -180,7 +180,7 @@ for name_fine in methods_fine:
             for Nf in Nf_v:
                 for Ng in Ng_v:
 
-                    optim.compile(u0, tau, tau, E_ref[beta],grad_type_coarse=name_coarse, grad_type_fine = name_fine, Nf = Nf, Ng = Ng)
+                    optim.compile(u0, E_ref[beta],grad_type_coarse=name_coarse, grad_type_fine = name_fine, Nf = Nf, Ng = Ng, tau_f = tau, tau_g = tau * Nf)
 
                     filename = 'PF'+ name_fine + '_' + name_coarse + '_tau' + str(tau) + '_Nf'+ str(Nf)+ '_Ng' + str(Ng)
                     if is_save_log:
@@ -188,7 +188,7 @@ for name_fine in methods_fine:
                         sys.stdout = f
                     res = optim.minimize(100, toll, is_save_log)
                     if is_save_plot:
-                        optim.plot_history(filesave = str(folder / (filename + '.png')))
+                        optim.plot_history(filesave = str(folder / (filename + '_old.png')))
                     if is_save_CSV:
                         optim.save_data(filename_results_PF, res)
                     if is_save_log: